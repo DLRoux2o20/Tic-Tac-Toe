fa4e79b2a634f7c1a7819b31c8b6d08524cd3c62
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-20 14:13:00 +0200

    Change many instances of Gameboard to the this keyword. Finish game for console.

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ let game = (function () {
 
 			Gameboard.board[row - 1][column - 1] = playersTurn.marker;
 			turnsplayed++;
-			this.showBoard();
+			Gameboard.showBoard();
 
 			if (turnsplayed >= 3) {
 				if (Gameboard.checkForWin(row, column) === "win") {
@@ -41,63 +41,56 @@ let game = (function () {
 		},
 		showBoard: function () {
 			console.log(
-				`${Gameboard.board[0].join(" ")}\n${Gameboard.board[1].join(
+				`${this.board[0].join(" ")}\n${this.board[1].join(
 					" "
-				)}\n${Gameboard.board[2].join(" ")}\n`
+				)}\n${this.board[2].join(" ")}\n`
 			);
 		},
 		checkForWin: function (row, column) {
-			// Verander later alle Gameboard na this
 			if (
-				Gameboard.board[row - 1][0] === Gameboard.board[row - 1][1] &&
-				Gameboard.board[row - 1][0] === Gameboard.board[row - 1][2]
+				this.board[row - 1][0] === this.board[row - 1][1] &&
+				this.board[row - 1][0] === this.board[row - 1][2]
 			) {
-				Gameboard.playerWon();
+				this.playerWon();
 				return "win";
 			} else if (
-				Gameboard.board[0][column - 1] === Gameboard.board[1][column - 1] &&
-				Gameboard.board[0][column - 1] === Gameboard.board[2][column - 1]
+				this.board[0][column - 1] === this.board[1][column - 1] &&
+				this.board[0][column - 1] === this.board[2][column - 1]
 			) {
-				Gameboard.playerWon();
+				this.playerWon();
 				return "win";
 			} else if (row === 2) {
 				if (
-					(Gameboard.board[0][0] === Gameboard.board[1][1] &&
-						Gameboard.board[0][0] === Gameboard.board[2][2]) ||
-					(Gameboard.board[0][2] === Gameboard.board[1][1] &&
-						Gameboard.board[0][2] === Gameboard.board[2][0])
+					(this.board[0][0] === this.board[1][1] &&
+						this.board[0][0] === this.board[2][2]) ||
+					(this.board[0][2] === this.board[1][1] &&
+						this.board[0][2] === this.board[2][0])
 				) {
-					Gameboard.playerWon();
+					this.playerWon();
 					return "win";
 				}
 				return;
 			} else if (row === 1) {
 				if (
 					column !== 2 &&
-					((Gameboard.board[0][column - 1] === Gameboard.board[1][column] &&
-						Gameboard.board[0][column - 1] ===
-							Gameboard.board[2][column + 1]) ||
-						(Gameboard.board[0][column - 1] ===
-							Gameboard.board[1][column - 2] &&
-							Gameboard.board[0][column - 1] ===
-								Gameboard.board[2][column - 3]))
+					((this.board[0][column - 1] === this.board[1][column] &&
+						this.board[0][column - 1] === this.board[2][column + 1]) ||
+						(this.board[0][column - 1] === this.board[1][column - 2] &&
+							this.board[0][column - 1] === this.board[2][column - 3]))
 				) {
-					Gameboard.playerWon();
+					this.playerWon();
 					return "win";
 				}
 				return;
 			} else if (row === 3) {
 				if (
 					column !== 2 &&
-					((Gameboard.board[2][column - 1] === Gameboard.board[1][column] &&
-						Gameboard.board[2][column - 1] ===
-							Gameboard.board[0][column + 1]) ||
-						(Gameboard.board[2][column - 1] ===
-							Gameboard.board[1][column - 2] &&
-							Gameboard.board[2][column - 1] ===
-								Gameboard.board[0][column - 3]))
+					((this.board[2][column - 1] === this.board[1][column] &&
+						this.board[2][column - 1] === this.board[0][column + 1]) ||
+						(this.board[2][column - 1] === this.board[1][column - 2] &&
+							this.board[2][column - 1] === this.board[0][column - 3]))
 				) {
-					Gameboard.playerWon();
+					this.playerWon();
 					return "win";
 				}
 			}
@@ -117,25 +110,16 @@ let game = (function () {
 			}
 			player1.marker = player1.marker === "X" ? "O" : "X";
 			player2.marker = player2.marker === "O" ? "X" : "O";
-			console.log(`Board Reset:\n`); // Verwyder later
-			this.showBoard(); // Verwyder later
+			console.log(`Board Reset:\n`);
+			this.showBoard();
 		},
 	};
 	Gameboard.init();
 
 	return {
 		playRound: Gameboard.playRound,
-		showBoard: Gameboard.showBoard,
-		checkForWin: Gameboard.checkForWin, // Verwyder later
 	};
 })();
-
-game.playRound(1, 3);
-game.playRound(3, 3);
-game.playRound(3, 1);
-game.playRound(3, 2);
-game.playRound(2, 2);
-game.playRound(3, 2);
 
 function createPlayer(marker, name) {
 	const score = 0;
