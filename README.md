3e071e92db1ca4256ed1f42c8be02ae4f95cd2ae
DLRoux2o20 <153112792+DLRoux2o20@users.noreply.github.com>
2025-12-21 17:13:13 +0200

    Rewrite code to be better and make each object - Game, Gameboard and Players - its own module or factory. Renamed many variables and functions. Rewrite checkForWin (or now isWinner) function if statements to be more concise and less code. Game finished for console.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Lessons learnt when making website:
 
-1. 
+1. const variables kan have a function as a value so that the value can change each time it is defined/read, but the variable can't be reassigned.
--- main.js
+++ main.js
@@ -1,145 +1,145 @@
-// VERWYDER LATER
-let game = (function () {
-	let Players = {
-		player1: createPlayer("X", "Duan"),
-		player2: createPlayer("O", "AI"),
-		turnsPlayed: 0,
-	};
+function createPlayer(name, marker) {
+	let score = 0;
 
-	Players.playersTurn = Players.player1;
-	Players.playerGoingFirst = Players.player1;
+	function addPoint() {
+		score++;
+	}
 
-	// WRAP GAMEBOARD OBJECT IN IIFE (MODULE)
-	let Gameboard = {
-		board: [
-			[".", ".", "."],
-			[".", ".", "."],
-			[".", ".", "."],
-		],
-		init: function () {
-			this.showBoard();
-		},
-		// GAME OBJECT
-		playRound: function (row, column) {
-			if (Players.turnsPlayed === 9) {
-				return;
-			}
-			if (Gameboard.board[row - 1][column - 1] !== ".") {
-				console.log("Try again");
-				return;
-			}
-
-			Gameboard.board[row - 1][column - 1] = Players.playersTurn.marker;
-			Players.turnsPlayed++;
-			Gameboard.showBoard();
-
-			if (Players.turnsPlayed >= 3) {
-				if (Gameboard.checkForWin(row, column) === "win") {
-					Players.playersTurn = Players.playerGoingFirst;
-					return;
-				}
-			}
-
-			Players.playersTurn =
-				Players.playersTurn === Players.player1
-					? Players.player2
-					: Players.player1;
-		},
-		showBoard: function () {
-			console.log(
-				`${this.board[0].join(" ")}\n${this.board[1].join(
-					" "
-				)}\n${this.board[2].join(" ")}\n`
-			);
-		},
-		// GAME OBJECT
-		checkForWin: function (row, column) {
-			if (
-				this.board[row - 1][0] === this.board[row - 1][1] &&
-				this.board[row - 1][0] === this.board[row - 1][2]
-			) {
-				this.playerWon();
-				return "win";
-			} else if (
-				this.board[0][column - 1] === this.board[1][column - 1] &&
-				this.board[0][column - 1] === this.board[2][column - 1]
-			) {
-				this.playerWon();
-				return "win";
-			} else if (row === 2) {
-				if (
-					(this.board[0][0] === this.board[1][1] &&
-						this.board[0][0] === this.board[2][2]) ||
-					(this.board[0][2] === this.board[1][1] &&
-						this.board[0][2] === this.board[2][0])
-				) {
-					this.playerWon();
-					return "win";
-				}
-			} else if (row === 1) {
-				if (
-					column !== 2 &&
-					((this.board[0][column - 1] === this.board[1][column] &&
-						this.board[0][column - 1] === this.board[2][column + 1]) ||
-						(this.board[0][column - 1] === this.board[1][column - 2] &&
-							this.board[0][column - 1] === this.board[2][column - 3]))
-				) {
-					this.playerWon();
-					return "win";
-				}
-			} else if (row === 3) {
-				if (
-					column !== 2 &&
-					((this.board[2][column - 1] === this.board[1][column] &&
-						this.board[2][column - 1] === this.board[0][column + 1]) ||
-						(this.board[2][column - 1] === this.board[1][column - 2] &&
-							this.board[2][column - 1] === this.board[0][column - 3]))
-				) {
-					this.playerWon();
-					return "win";
-				}
-			}
-
-			if (Players.turnsPlayed === 9) {
-				console.log("It's a draw!");
-				this.resetGame();
-			}
-		},
-		// GAME OBJECT
-		playerWon: function () {
-			Players.playersTurn.score += 1;
-			Players.playerGoingFirst =
-				Players.playerGoingFirst === Players.player1
-					? Players.player2
-					: Players.player1;
-			console.log(`${Players.playersTurn.name} Won!`);
-			console.log(
-				`${Players.playersTurn.name}'s score is ${Players.playersTurn.score}`
-			);
-			this.resetGame();
-		},
-		// GAME OBJECT
-		resetGame: function () {
-			for (let i = 0; i < 3; i++) {
-				for (let j = 0; j < 3; j++) {
-					this.board[i][j] = ".";
-				}
-			}
-			Players.player1.marker = Players.player1.marker === "X" ? "O" : "X";
-			Players.player2.marker = Players.player2.marker === "O" ? "X" : "O";
-			console.log(`Board Reset:\n`);
-			this.showBoard();
-		},
-	};
-	Gameboard.init();
+	function getScore() {
+		return score;
+	}
 
 	return {
-		playRound: Gameboard.playRound,
+		name,
+		marker,
+		addPoint,
+		getScore,
+	};
+}
+
+let Gameboard = (function () {
+	const board = [
+		[".", ".", "."],
+		[".", ".", "."],
+		[".", ".", "."],
+	];
+
+	function getBoard() {
+		return board.map((row) => [...row]);
+	}
+
+	function setCell(row, col, marker) {
+		if (board[row][col] === ".") {
+			board[row][col] = marker;
+			return true;
+		}
+		console.log("Try again");
+		return false;
+	}
+
+	function showBoard() {
+		console.log(board.map((row) => row.join(" ")).join("\n"));
+	}
+
+	function resetBoard() {
+		board.forEach((row, i) => row.forEach((e, j) => (board[i][j] = ".")));
+		console.log(`Board Reset:\n`);
+		showBoard();
+	}
+
+	showBoard();
+
+	return {
+		getBoard,
+		setCell,
+		showBoard,
+		resetBoard,
 	};
 })();
 
-// SKYF MISKIEN
-function createPlayer(marker, name) {
-	const score = 0;
-	return { name, score, marker };
-}
+let Game = (function () {
+	let player1 = createPlayer("Duan", "X");
+	let player2 = createPlayer("AI", "O");
+	let playersTurn = player1;
+	let playerGoingFirst = player1;
+	let turnsPlayed = 0;
+
+	function playRound(row, col) {
+		row -= 1;
+		col -= 1;
+
+		if (turnsPlayed === 9) {
+			return;
+		}
+
+		Gameboard.setCell(row, col, playersTurn.marker);
+		turnsPlayed++;
+		Gameboard.showBoard();
+
+		const boardSnapshot = Gameboard.getBoard();
+
+		if (turnsPlayed >= 5) {
+			if (isWinner(boardSnapshot, row, col, playersTurn.marker)) {
+				playersTurn = playerGoingFirst;
+				return;
+			}
+		}
+
+		playersTurn = playersTurn === player1 ? player2 : player1;
+	}
+
+	function isWinner(board, row, col, marker) {
+		if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
+			playerWon();
+			return true;
+		} else if (
+			board[0][col] === board[1][col] &&
+			board[0][col] === board[2][col]
+		) {
+			playerWon();
+			return true;
+		}
+
+		if (
+			board[0][0] === marker &&
+			board[1][1] === marker &&
+			board[2][2] === marker
+		) {
+			playerWon();
+			return true;
+		}
+		if (
+			board[0][2] === marker &&
+			board[1][1] === marker &&
+			board[2][0] === marker
+		) {
+			playerWon();
+			return true;
+		}
+
+		if (turnsPlayed === 9) {
+			console.log("It's a draw!");
+			resetGame();
+		}
+		return false;
+	}
+
+	function playerWon() {
+		playersTurn.addPoint();
+		console.log(`${playersTurn.name} Won!`);
+		console.log(`${playersTurn.name}'s score is ${playersTurn.getScore()}`);
+		resetGame();
+	}
+
+	function resetGame() {
+		playerGoingFirst = playerGoingFirst === player1 ? player2 : player1;
+		player1.marker = player1.marker === "X" ? "O" : "X";
+		player2.marker = player2.marker === "O" ? "X" : "O";
+		turnsPlayed = 0;
+		Gameboard.resetBoard();
+	}
+
+	return {
+		playRound,
+	};
+})(Gameboard);
